09fba14b109b13ee43ecde1877dfa87533e85ec0
VARUN RAJ <varunraj82@gmail.com>
2014-03-28 20:18:16 -0500

    Added setup.sh to start the server

---
+++ setup.sh
@@ -0,0 +1,8 @@
+sudo apt-get install python-software-properties
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install nodejs
+sudo apt-get install git
+git clone https://github.com/varunraj/user_management.git
+
+
